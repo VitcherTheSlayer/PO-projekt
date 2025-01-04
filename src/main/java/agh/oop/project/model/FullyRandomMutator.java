70a1d1d44581700ed7ae43f9c0a759e549d87916
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-01-04 22:40:52 +0100

    Create FullyRandomMutator.java

---
+++ src/main/java/agh/oop/project/model/FullyRandomMutator.java
@@ -0,0 +1,42 @@
+package agh.oop.project.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class FullyRandomMutator implements Mutator {
+
+    protected final int minMutation;
+    protected final int maxMutation;
+    protected final Random rng = new Random();
+
+    public FullyRandomMutator(int minMutation, int maxMutation) {
+        this.maxMutation = maxMutation;
+        this.minMutation = minMutation;
+    }
+
+    protected Genome mutate(ArrayList<Integer> rawGenome){
+        final int n = rng.nextInt(minMutation, maxMutation + 1);
+
+        List<Integer> genesIdxs = new ArrayList<>();
+        for(int i = 0; i < rawGenome.size(); i++){
+            genesIdxs.add(i);
+        }
+        Collections.shuffle(genesIdxs, rng);
+
+        for(int i : genesIdxs
+                .stream()
+                .limit(n)
+                .toList()){
+            rawGenome.set(i, rng.nextInt(Genome.GENES_COUNT));
+        }
+
+        return new Genome(rawGenome);
+    }
+
+    @Override
+    public Genome mutate(Genome genome) {
+        return mutate(genome.copyValues());
+    }
+}
