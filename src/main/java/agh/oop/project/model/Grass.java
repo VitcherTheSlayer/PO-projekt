a7da7887611dbaeba15ca74ab9bdd2d286a67ba3
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-01-13 16:25:18 +0100

    Create Grass.java

---
+++ src/main/java/agh/oop/project/model/Grass.java
@@ -0,0 +1,19 @@
+package agh.oop.project.model;
+
+public class Grass implements MapEntity{
+    private final Vector2d position;
+
+    public Grass(Vector2d position){
+        this.position = position;
+    }
+
+    @Override
+    public Vector2d getPosition() {
+        return position;
+    }
+
+    @Override
+    public boolean isAt(Vector2d position) {
+        return this.position.equals(position);
+    }
+}
