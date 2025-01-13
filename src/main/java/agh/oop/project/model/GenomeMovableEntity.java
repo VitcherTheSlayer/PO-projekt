51346124b58bcf0faeba69d836b6756342115858
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-01-13 16:25:24 +0100

    Create GenomeMovableEntity.java

---
+++ src/main/java/agh/oop/project/model/GenomeMovableEntity.java
@@ -0,0 +1,38 @@
+package agh.oop.project.model;
+
+import java.util.Iterator;
+
+public abstract class GenomeMovableEntity {
+    protected final Genome genome;
+    protected Iterator<Integer> genomeIterator;
+    protected Vector2d position;
+    protected Rotation rotation;
+
+    public GenomeMovableEntity(Genome genome, Vector2d position) {
+        this.genome = genome;
+        this.position = position;
+        genomeIterator = this.genome.iterator();
+    }
+    // random
+    public GenomeMovableEntity(int genomeLength, Vector2d position) {
+        genome = new Genome(genomeLength);
+        this.position = position;
+        genomeIterator = this.genome.iterator();
+    }
+
+    public void move(MoveValidator moveValidator) {
+        rotation = rotation.add(genomeIterator.next());
+
+        var nextMove = moveValidator.requestMove(this, position.add(rotation.nextMove()));
+        position = nextMove.getKey();
+        rotation = nextMove.getValue();
+    }
+
+    public Rotation getRotation() {
+        return rotation;
+    }
+
+    public Vector2d getPosition() {
+        return position;
+    }
+}
