7271486e2aae04abc60416d65ecbbb06d6fb6db3
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-01-15 18:03:28 +0100

    Update Animal.java

--- src/main/java/agh/oop/project/model/Animal.java
+++ src/main/java/agh/oop/project/model/Animal.java
@@ -1,17 +1,93 @@
 package agh.oop.project.model;
 
-public class Animal extends GenomeMovableEntity{
-    private int energy;
+import java.util.*;
 
-    public Animal(Genome genome, Vector2d position) {
-        super(genome, position);
+public class Animal extends GenomeMovableEntity implements Comparable<Animal> {
+    private int energy;
+    private AbstractMap map;
+    private int age = 0;
+    private final List<Animal> children = new ArrayList<>();
+    private int grassEaten = 0;
+    private int deathTime = -1;
+    private String deathReason = "";
+
+    public Animal(Genome genome, Vector2d position, Rotation rotation, AbstractMap map) {
+        super(genome, position, rotation);
+        this.map = map;
     }
 
-    public Animal(int genomeLength, Vector2d position) {
-        super(genomeLength, position);
+    public Animal(int genomeLength, Vector2d position, Rotation rotation, AbstractMap map) {
+        super(genomeLength, position, rotation);
+        this.map = map;
     }
 
     public int getEnergy() {
         return energy;
+    }
+
+    // called once per tile by the dominant Animal
+    public Animal breedWith(Animal other) {
+        if(!breedable() || !other.breedable()) {
+            return null;
+        }
+
+        energy -= map.getConfiguration().reproductionEnergyUsage();
+        other.energy -= map.getConfiguration().reproductionEnergyUsage();
+
+        Genome newGenome = map
+                .getMutator()
+                .mutate(GenomeCombiner
+                        .combine(
+                                genome,
+                                other.genome,
+                                energy,
+                                other.energy
+                        )
+                );
+        Rotation newRotation = Rotation.NORTH.add(new Random().nextInt(Genome.UNIQUE_GENES_COUNT));
+
+        return new Animal(newGenome, position, newRotation, map);
+    }
+
+    @Override
+    public int compareTo(Animal o) {
+        return energy - o.energy;
+    }
+
+    @Override
+    public void move(MoveValidator moveValidator) {
+        super.move(moveValidator);
+        ++age;
+    }
+
+    public void eatGrass(){
+        energy += map.getConfiguration().energyPerPlant();
+        ++grassEaten;
+    }
+
+    public float energyFraction() {
+        return Math.min(1.0f, (float)energy / (float)map.getConfiguration().initialEnergy());
+    }
+
+    public boolean breedable() {
+        return energy >= map.getConfiguration().reproductionMinEnergy();
+    }
+
+    public void die(int time, String reason) {
+        energy = 0;
+        this.deathTime = time;
+        this.deathReason = reason;
+    }
+
+    public int childrenCount() {
+        return children.size();
+    }
+
+    public int descendantsCount() {
+        int count = 0;
+        for(Animal child : children) {
+            count += child.descendantsCount() + 1;
+        }
+        return count;
     }
 }
