44a7783be0418b768db79c13182e51736dbe161c
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-01-15 18:04:33 +0100

    Create GlobeMap.java

---
+++ src/main/java/agh/oop/project/model/GlobeMap.java
@@ -0,0 +1,28 @@
+package agh.oop.project.model;
+
+import javafx.util.Pair;
+
+public class GlobeMap extends AbstractMap {
+    protected GlobeMap(Configuration configuration) {
+        super(configuration);
+    }
+
+    @Override
+    public Pair<Vector2d, Rotation> requestMove(GenomeMovableEntity genomeMovableEntity, Vector2d requestedPosition) {
+        int newX = requestedPosition.getX();
+        if(newX < boundary.lowerLeft().getX()){
+             newX = boundary.upperRight().getX();
+        }
+        else if(newX > boundary.upperRight().getX()){
+            newX = boundary.lowerLeft().getX();
+        }
+
+        Rotation newRotation = genomeMovableEntity.getRotation();
+        int newY = requestedPosition.getY();
+        if(newY < boundary.lowerLeft().getY() || newY > boundary.upperRight().getY()){
+            newRotation = newRotation.add(Genome.UNIQUE_GENES_COUNT);
+        }
+
+        return new Pair<>(new Vector2d(newX, newY), newRotation);
+    }
+}
