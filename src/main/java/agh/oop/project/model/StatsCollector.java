e7b458deeee573be861543e0031d837fe1bd8e76
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2025-01-28 22:01:21 +0100

    Update StatsCollector.java

--- src/main/java/agh/oop/project/model/StatsCollector.java
+++ src/main/java/agh/oop/project/model/StatsCollector.java
@@ -10,59 +10,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StatsCollector {
-    AbstractMap worldMap;
-    // Lista wartości reprezentujących dni
-    private final List<Integer> days = new ArrayList<>();
+    private final File file;
 
-    // Zależności danych od dnia
-    private final List<Integer> animalCounts = new ArrayList<>();
-    private final List<Integer> plantCounts = new ArrayList<>();
-    private final List<Integer> freeFields = new ArrayList<>();
-    private final List<Double> averageEnergyLevels = new ArrayList<>();
-    private final List<Double> averageLifespams = new ArrayList<>();
+    private static final String dayLabel = "Day";
+    private static final String animalCountLabel = "Animal Count";
+    private static final String plantCountLabel = "Plant Count";
+    private static final String freeFieldsLabel = "Free Fields";
+    private static final String dominantGenomeLabel = "Dominant Genome";
+    private static final String averageEnergyLabel = "Average Energy";
+    private static final String averageLifespanLabel = "Average Lifespan";
+    private static final String averageChildrenCountLabel = "Average Children Count";
 
-    public StatsCollector(AbstractMap worldMap) {
-        this.worldMap = worldMap;
-    }
+    public StatsCollector(File file) throws IOException {
+        this.file = file;
 
-    public void addData(AbstractMap worldMap,int day) {
-        days.add(day);
-        animalCounts.add(worldMap.getAnimals().size());
-        plantCounts.add(worldMap.getPlantsAmount());
-        freeFields.add(worldMap.getAllFreeCells());
-        averageEnergyLevels.add(worldMap.getAverageEnergy());
-        averageLifespams.add(worldMap.getAverageLifeSpan());
-    }
-
-    public void exportToCsv() throws IOException {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Save Simulation Stats");
-
-        FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("CSV files (*.csv)", "*.csv");
-        fileChooser.getExtensionFilters().add(filter);
-
-        fileChooser.setInitialDirectory(Paths.get(".").toFile());
-
-        File file = fileChooser.showSaveDialog(null);
-
-        if (file != null) {
-            try (FileWriter writer = new FileWriter(file)) {
-                // Nagłówki kolumn
-                writer.write("Day,Animal Count,Plant Count,Free Fields,Average Energy,Average Lifespam\n");
-
-                // Dane
-                for (int i = 0; i < days.size(); i++) {
-                    writer.write(String.format(
-                            "%d,%d,%d,%d,%.2f,%.2f\n",
-                            days.get(i),
-                            animalCounts.get(i),
-                            plantCounts.get(i),
-                            freeFields.get(i),
-                            averageEnergyLevels.get(i),
-                            averageLifespams.get(i)
-                    ));
-                }
-            }
+        try(FileWriter fileWriter = new FileWriter(file)) {
+            fileWriter.write("%s,%s,%s,%s,%s,%s,%s,%s\n".formatted(
+                    dayLabel,
+                    animalCountLabel,
+                    plantCountLabel,
+                    freeFieldsLabel,
+                    dominantGenomeLabel,
+                    averageEnergyLabel,
+                    averageLifespanLabel,
+                    averageChildrenCountLabel
+            ));
+            fileWriter.flush();
         }
+        catch (NullPointerException ignored) {}
     }
 }
